30cf880a0a84f205b651b56a72cd0637568d5bbd
LL <microyang034@outlook.com>
2022-01-14 16:15:25 +0800

    day11 homework update commit

--- day11/day11作业3 文件拷贝.py
+++ day11/day11作业3 文件拷贝.py
@@ -9,5 +9,8 @@ src_file = input("输入源文件地址：").strip()
 dst_file = input("输入目的文件地址：").strip()
 with open(r"{}".format(src_file),mode="rt",encoding="utf-8") as f1, \
 open(r"{}".format(dst_file),mode="wt",encoding="utf-8") as f2:
-    res = f1.read()
-    f2.write(res)
+    # res = f1.read()
+    # f2.write(res)
+    # 当数据量大的时候不能这么写
+    for line in f1:
+        f2.write(line)
